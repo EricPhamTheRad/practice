d09ce19a7f437b598ae5b2c04facd617d18e20af
EricTheRad <94642571+EricPhamTheRad@users.noreply.github.com>
2021-11-18 20:18:40 +0000

    Takes an string, makes a file and put the string into the file

--- main.c
+++ main.c
@@ -2,35 +2,23 @@
 #include <string.h>
 #include <ctype.h>
 
-void addnum(int num1, int num2, int* sum){
-  *sum = num1 + num2;
-}
-
 int main(void) {
   FILE* inFile = NULL;
-  int num1;
-  int num2;
-  int sum;
-  char name[4][25];
+  
+  char input[100];
   char filename[100];
+
+  fgets(input, 101, stdin);
 
   fscanf(stdin, "%s", filename);
 
   inFile = fopen(strcat(filename, ".txt"), "w");
    if (inFile == NULL) {
       printf("Could not open file myfile.txt.\n");
-      return -1; // -1 indicates error
    }
-  /*
-  for(int i = 0; i < 4; i++){
-    fscanf(inFile, "%s", name[i]);
-  }
 
-  //scanf("%d %d", &num1, &num2);
-  //addnum(num1, num2, &sum);
-  for(int i = 0; i < 4; i++){
-    printf("%s ", name[i]);
-  }
-  */
-return 0;
+  fputs(input, inFile);  
+
+  
+  return 0;
 }
