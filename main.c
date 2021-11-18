793aa7340aafb97d618ee540976c0adc2e7f789c
Eric Pham <94642571+EricPhamTheRad@users.noreply.github.com>
2021-11-18 19:29:08 +0000

    Add two numbers with pointer

--- main.c
+++ main.c
@@ -2,7 +2,16 @@
 #include <string.h>
 #include <ctype.h>
 
+void addnum(int num1, int num2, int* sum){
+  *sum = num1 + num2;
+}
+
 int main(void) {
-  
+  int sum;
+  int num1;
+  int num2;
+  scanf("%d %d", &num1, &num2);
+  addnum(num1, num2, &sum);
+  printf("%d", sum);
 return 0;
 }
